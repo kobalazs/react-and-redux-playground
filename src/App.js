32beecc689bc94de8ac3dff1c581c24ff2f5cf94
Balázs Kovács <kobalazs@gmail.com>
2020-09-04 20:18:57 +0200

    Add Registration, Login & NotFound components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink
 } from 'react-router-dom';
+
+import './App.css';
 import Home from './features/common/Home';
 import Counter from './features/counter/Counter';
+import Registration from './features/user/Registration';
+import Login from './features/user/Login';
+import NotFound from './features/common/NotFound';
 
 export default function App() {
   return (
@@ -16,6 +20,8 @@ export default function App() {
         <strong>React Playground</strong>
         <NavLink to="/" activeClassName="active" exact>Home</NavLink>
         <NavLink to="/counter" activeClassName="active">Counter</NavLink>
+        <NavLink to="/registration" activeClassName="active">Registration</NavLink>
+        <NavLink to="/login" activeClassName="active">Login</NavLink>
       </nav>
       <div className="container">
         <Switch>
@@ -24,6 +30,15 @@ export default function App() {
           </Route>
           <Route path="/counter">
             <Counter />
+          </Route>
+          <Route path="/registration">
+            <Registration />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </div>
