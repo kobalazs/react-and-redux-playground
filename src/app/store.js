b36702862d885a4649523aca7eac2fb57b7514c2
Balázs Kovács <kobalazs@gmail.com>
2020-09-08 21:15:44 +0200

    Implement store persistence using redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 import authReducer from '../features/user/authSlice';
 import counterReducer from '../features/counter/counterSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
-  reducer: {
-    auth: authReducer,
-    counter: counterReducer
-  },
+const reducers = combineReducers({
+  auth: authReducer,
+  counter: counterReducer
 });
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+});
+const persistor = persistStore(store);
+
+export default store;
+export { persistor };
