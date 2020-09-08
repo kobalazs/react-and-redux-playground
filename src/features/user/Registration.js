63bcad80bec5f4b9bb8a65a3dd49adfa0044a487
Balázs Kovács <kobalazs@gmail.com>
2020-09-08 09:31:26 +0200

    Validate registration using React Hook Form & turn the form into an uncontrolled component

--- src/features/user/Registration.js
+++ src/features/user/Registration.js
@@ -1,16 +1,12 @@
 import React, { Fragment } from 'react';
 
 import UserDto from './UserDto';
+import RegistrationForm from './RegistrationForm';
 
 export default class Registration extends React.Component {
   constructor() {
     super();
-    this.state = {
-      user: new UserDto(),
-    };
-    this.changeUser = this.changeUser.bind(this);
     this.register = this.register.bind(this);
-    this.reset = this.reset.bind(this);
   }
 
   changeUser(event) {
@@ -18,36 +14,16 @@ export default class Registration extends React.Component {
     this.setState(Object.assign({}, this.state, { user }));
   }
 
-  register(event) {
-    event.preventDefault();
-    console.log('Register', this.state.user);
-  }
-
-  reset() {
-    this.setState(Object.assign({}, this.state, { user: new UserDto() }));
+  register(data) {
+    const user = new UserDto(data);
+    console.log('Register', user);
   }
 
   render() {
     return (
       <Fragment>
         <h1>Registration</h1>
-        <pre>{JSON.stringify(this.state.user, null, 2)}</pre>
-        <form onSubmit={this.register} onReset={this.reset}>
-          <label>
-            Name
-            <input name="name" type="text" value={this.state.user.name ?? ''} onChange={this.changeUser} />
-          </label>
-          <label>
-            Email
-            <input name="email" type="email" value={this.state.user.email ?? ''} onChange={this.changeUser} />
-          </label>
-          <label>
-            Password
-            <input name="password" type="password" value={this.state.user.password ?? ''} onChange={this.changeUser} />
-          </label>
-          <button type="submit">Register</button>
-          <button type="reset">Reset</button>
-        </form>
+        <RegistrationForm onSubmit={this.register} />
       </Fragment>
     );
   }
---
+++ src/features/user/RegistrationForm.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function RegistrationForm(props) {
+  const { register, handleSubmit, errors, getValues } = useForm();
+  const validateAndSubmit = event => {
+    event.preventDefault();
+    handleSubmit(props.onSubmit)(event);
+  };
+  const matchesPreviousPassword = value => {
+    const { password } = getValues();
+    return password === value || 'Passwords should match!';
+  };
+  return (
+    <form onSubmit={validateAndSubmit}>
+      <label>
+        Name
+        <input
+          name="name"
+          type="text"
+          ref={register({ required: 'Name is required!' })}
+        />
+        {errors.name && <span className="error">{errors.name.message}</span>}
+      </label>
+      <label>
+        Email
+        <input
+          name="email"
+          type="email"
+          ref={register({
+            required: 'Email is required!',
+            pattern: {
+              value: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
+              message: 'Email should be valid!'
+            }
+          })}
+        />
+        {errors.email && <span className="error">{errors.email.message}</span>}
+      </label>
+      <label>
+        Password
+        <input
+          name="password"
+          type="password"
+          ref={register({
+            required: 'Password is required!',
+            minLength: {
+              value: 6,
+              message: 'Password should be at least 6 characters long!'
+            }
+          })}
+        />
+        {errors.password && <span className="error">{errors.password.message}</span>}
+      </label>
+      <label>
+        Confirm Password
+        <input
+          name="passwordConfirmation"
+          type="password"
+          ref={register({
+            required: 'Please confirm your password!',
+            validate: { matchesPreviousPassword }
+          })}
+        />
+        {errors.passwordConfirmation && <span className="error">{errors.passwordConfirmation.message}</span>}
+      </label>
+      <button type="submit">Register</button>
+      <button type="reset">Reset</button>
+    </form>
+  );
+}
